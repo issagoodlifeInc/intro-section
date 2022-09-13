cbe4bd090a2cb81985c0c99c9312f7ec39e9b470
Lesley <leskimuti@gmail.com>
2022-09-13 12:27:58 +0300

    Basic setup of the section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,13 @@
+import heroMobile from "../images/image-hero-mobile.png";
+import heroDesktop from "../images/image-hero-desktop.png";
+
 export default function Main() {
-  <main>
-    Make remote work Get your team in sync, no matter your location. Streamline
-    processes, create team rituals, and watch productivity soar. Learn more
-  </main>;
+  return (
+    <main>
+      <div className="main--img">pict</div>
+      Make remote work Get your team in sync, no matter your location.
+      Streamline processes, create team rituals, and watch productivity soar.
+      Learn more
+    </main>
+  );
 }
